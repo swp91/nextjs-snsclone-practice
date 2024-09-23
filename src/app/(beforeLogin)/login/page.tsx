e740f42e9abe301d0444f769dddb2e5681116053
swp91 <seongwoo4477@gmail.com>
2024-09-24 06:49:54 +0900

    로그인 모달에서 발생하는 문제 해결하기(router.replace)

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -8,3 +8,9 @@ export default function Login() {
   router.replace("/i/flow/login");
   return <Main />;
 }
+
+// router.push
+// localhost:3000/login -> localhost:3000/i/flow/login
+
+//router.replace (replace는 이전 히스토리를 없애버린다(대체한다))
+// localhost:3000/login -> localhost:3000/i/flow/login
