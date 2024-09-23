28faf6557f5be6dbf8f398c348c40ee995848dce
swp91 <seongwoo4477@gmail.com>
2024-09-24 01:59:16 +0900

    default.tsx

--- src/app/(beforeLogin)/@modal/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <div>패러렐 모달123</div>;
-}
--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -11,3 +11,6 @@ export default function Layout({ children, modal }: Props) {
     </div>
   );
 }
+
+//주소가 localhost:3000일때는 children -> page.tsx, modal -> @modal/default.tsx
+//주소가 localhost:3000/i/flow/login 때는 children -> i/flow/login/page.tsx , modal->@modal/i/flow/login/page.tsx
